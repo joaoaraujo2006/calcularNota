7a3c10834f69e8494dab41c6fb9f0078dd65e702
João Pedro Gonçalves <joao.araujo@sou.inteli.edu.br>
2025-02-10 11:53:40 -0300

    second commit

--- script.js
+++ script.js
@@ -2,16 +2,21 @@
 var nota = [10, 7, 8];
 var cont = 0;
 
+var maior = 0;
+var menor = 11;
 for (i of nota){
-    var maior = 0;
     cont++;
     console.log("A nota " + cont + " é: " + i);
     if (i > maior){
         maior = i;
     }
+    if (i < menor){
+        menor = i;
+    }
 }
 
 console.log("A maior nota é " + maior)
+console.log("A menor nota é " + menor)
 var media = (nota[0] + nota[1] + nota[2]) / 3;
 
 function calcularResultado(){
